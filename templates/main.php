992f3e96b259398a9fbdfcf23903d23d54e5b222
Thomas <thomas@localhost.localdomain>
2025-03-21 12:17:24 +0100

    update

--- templates/main.php
+++ templates/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Accounting</title>
+    <title>Cats Organization</title>
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/style.css" rel="stylesheet">
 </head>
